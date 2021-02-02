01a4905d36df69781797e5d8cfec0506420b4ad5
RyadaProductions <RyadaProductions@users.noreply.github.com>
2021-02-02 14:23:54 +0100

    Fix issue with deserializing the response from steam api

--- RyaUploader.Core/Services/SteamServices/SteamApi.cs
+++ RyaUploader.Core/Services/SteamServices/SteamApi.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using RyaUploader.Core.Facade;
 using RyaUploader.Core.Models;
 using Serilog;
@@ -11,7 +11,7 @@ namespace RyaUploader.Core.Services.SteamServices
     public class SteamApi : ISteamApi
     {
         private readonly IHttpClient _client;
-        // TODO: change this to something else when commiting
+        // TODO: Change this key to be invalid
         private const string KEY = "3C97DE56AF018917F183FB805DA6B17B";
         
         public SteamApi(IHttpClient client)
@@ -23,21 +23,21 @@ namespace RyaUploader.Core.Services.SteamServices
         {
             Log.Information($"Getting the steamProfiles for {ids}");
 
-
             var response = await _client.GetStringAsync(
                 $"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={KEY}&steamids={string.Join(",", ids)}");
-            var apiresponse = JObject.Parse(response);
 
-            var playerProfiles = apiresponse["response"]["players"].Children().Values<PlayerProfile>().ToArray();
-
-            var result = new Dictionary<long, PlayerProfile>();
-
-            for (var i = 0; i < 10; i++)
+            // Use anonymous type to unwrap the playerprofiles json
+            var anonymousDefinition = new
             {
-                result.Add(playerProfiles[i].SteamId, playerProfiles[i]);
-            }
+                response = new
+                {
+                    players = new List<PlayerProfile>()
+                }
+            };
 
-            return result;
+            var deserializedAnonymousType = JsonConvert.DeserializeAnonymousType(response, anonymousDefinition);
+
+            return deserializedAnonymousType.response.players.ToDictionary(x => x.SteamId);
         }
     }
 }
